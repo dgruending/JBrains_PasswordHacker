4ff580c29596e5f5ea4c10a7b9993bab3fd42146
dgruending <dgruending23@gmail.com>
2021-10-05 08:55:28 +0000

    Added argument parser for input

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,2 +1,25 @@
-# write your code here
+import argparse
 
+# Stage 1/5: Establishing a connection
+# Input [command line]:
+#   1. IP address
+#   2. port
+#   3. message for sending
+# Requirements:
+#   1. Create a new socket.
+#   2. Connect to a host and a port using the socket.
+#   3. Send a message from the third command line argument to the host using the socket.
+#   4. Receive the server’s response.
+#   5. Print the server’s response.
+#   6. Close the socket.
+
+
+def stage1():
+    parser = argparse.ArgumentParser("This program connects to an IP address and prints the response")
+    parser.add_argument("ip", help="IP address for connection")
+    parser.add_argument("port", help="Port for connection")
+    parser.add_argument("message", help="Message for sending")
+
+
+if __name__ == '__main__':
+    stage1()
