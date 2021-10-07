cb6c29d4fbe3ae42515530c45dcc10a7cdd96bee
dgruending <dgruending23@gmail.com>
2021-10-07 11:30:55 +0000

    Added stage 4

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -65,7 +65,28 @@ def stage3():
     print(password)
 
 
+# Stage 4/5:  Catching exception
+# Input [command line]:
+#   1. IP address
+#   2. port
+# Requirements:
+#   1. Parses the command line and gets two arguments that are IP address and port.
+#   2. Try all logins with an empty password.
+#   3. When you find the login, try out every possible password of length 1.
+#   4. When an exception occurs, you know that you found the first letter of the password.
+#   5. Use the found login and the found letter to find the second letter of the password.
+#   6. Repeat until you receive the ‘success’ message.
+#   7. Print the combination of login and password in JSON format.
+
+def stage4():
+    parser = argparse.ArgumentParser("This program connects to an IP address and prints the response")
+    parser.add_argument("ip", help="IP address for connection")
+    parser.add_argument("port", help="Port for connection", type=int)
+    args = parser.parse_args()
+
+
 if __name__ == '__main__':
     # stage1()
     # stage2()
-    stage3()
+    # stage3()
+    stage4()
